7fc0a28f9d68adc78be6a87629c0178bab288681
PedroSolov <phsolovjovaspinheiro@gmail.com>
2025-05-20 21:40:02 -0300

    Delete crudPI directory

--- crudPI/src/main/java/com/example/crudPI/controller/CreatUserDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.crudPI.controller;
-
-public record CreatUserDto(String name, String email, String password, String cep, String phone, String cpf) {
-}
--- crudPI/src/main/java/com/example/crudPI/controller/UpdateUserDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.crudPI.controller;
-
-public record UpdateUserDto(String name, String email, String password, String cep, String phone) {
-}
